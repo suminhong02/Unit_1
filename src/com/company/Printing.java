99479469c772d867b693b7860582bdf1234ab028
suminhong02 <sumin.hong@ssfs.org>
2019-09-10 15:00:26 -0400

    Completed 1.1 part I

--- src/com/company/Printing.java
+++ src/com/company/Printing.java
@@ -6,9 +6,9 @@ public class Printing {
 
         System.out.println("2:43");
         System.out.println("09/10/2019");
-        System.out.println("It is a great\n time to be a\n computer programmer!");
-        System.out.print("It is a great");
-        System.out.print("time to be a");
+        System.out.println("It is a great\ntime to be a\ncomputer programmer!");
+        System.out.print("It is a great ");
+        System.out.print("time to be a ");
         System.out.print("computer programmer!");
 
     }
