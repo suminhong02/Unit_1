72e067f96adc4d46e536420eec38d84e1f459177
suminhong02 <sumin.hong@ssfs.org>
2019-09-12 08:57:12 -0400

    1.1 Daily Exercises Part II #7

---
+++ src/com/company/Meta.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Meta {
+
+    public static void main(String[] arg) {
+
+        System.out.println("public class Hello {");
+        System.out.println("public static void main(String[] args) {");
+
+    }
+}
