3da1c3c97649a1682ab6fcb5ef99009f7b5aeecf
suminhong02 <sumin.hong@ssfs.org>
2019-09-13 09:49:15 -0400

    Completed Correcting Syntax Errors

---
+++ src/com/company/Problems2.java
@@ -0,0 +1,20 @@
+package com.company;
+// ********************************************
+//   Problems.java
+//
+//   There are several error in the code below. Fix them all to get things to run
+//   Hopefully you can see that IDEA can be a huge help with this.
+//
+// ********************************************
+
+public class Problems {
+
+    public static void main(String[] args) {
+
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("This program used to have lots of problems,");
+        System.out.println("but if it prints this, you fixed them all.");
+        System.out.println("         	*** Hurray! ***");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+}
