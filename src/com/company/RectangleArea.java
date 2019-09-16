173d4c5b2d403568ea927d55ac1501f1ebf00de1
suminhong02 <sumin.hong@ssfs.org>
2019-09-16 10:52:26 -0400

    Completed RectangleArea

---
+++ src/com/company/RectangleArea.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class RectangleArea {
+
+    public static void main(String[] args) {
+
+        int length = 14;
+        int width = 6;
+        int area = length * width;
+        System.out.println("Area = "+area);
+    }
+}
