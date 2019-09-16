969d4ef205d073205a3d499a3410131648e40d61
suminhong02 <sumin.hong@ssfs.org>
2019-09-16 11:18:43 -0400

    Completed TempConversion

---
+++ src/com/company/TempConversion.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class TempConversion {
+
+    public static void main(String [] args){
+
+        double Fahrenheit = 80.0;
+        double Celsius = (Fahrenheit-32)*5/9;
+        System.out.println(Fahrenheit+"°F = "+Celsius+"°C");
+    }
+}
