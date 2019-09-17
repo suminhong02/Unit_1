eaa7c81edae6b7edb2dfa2b7ddd82e8eac604abf
suminhong02 <sumin.hong@ssfs.org>
2019-09-17 12:44:33 -0400

    Completed Scanner RectangleArea

--- src/com/company/RectangleArea.java
+++ src/com/company/RectangleArea.java
@@ -1,12 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class RectangleArea {
 
     public static void main(String[] args) {
 
-        int length = 14;
-        int width = 6;
-        int area = length * width;
-        System.out.println("Area = "+area);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the length of Rectangle");
+        int length = scan.nextInt();
+        System.out.println("Enter the width of Rectangle");
+        int width = scan.nextInt();
+        final int area = length * width;
+        System.out.println("Area of Rectangle = "+area);
     }
+
 }
