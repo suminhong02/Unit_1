2c3f1760b65d95aa25ec7e5b54c0985d79fc82fd
suminhong02 <sumin.hong@ssfs.org>
2019-09-17 12:39:42 -0400

    Completed Scanner PoundsToKilograms

--- src/com/company/PoundsToKilograms.java
+++ src/com/company/PoundsToKilograms.java
@@ -1,11 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class PoundsToKilograms {
 
     public static void main(String[] args){
 
-       double Ibs = 115.0;
-        double kg = Ibs/2.205;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the weight in pounds to convert it into kilograms");
+        double Ibs = scan.nextInt();
+        final double kg = Ibs/2.205;
         System.out.println(Ibs+" pounds = " +kg+" kilograms");
 
     }
