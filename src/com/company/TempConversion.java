7e923fc0cc194c17deb96b0ea423ffbabc081eab
suminhong02 <sumin.hong@ssfs.org>
2019-09-17 12:48:09 -0400

    Completed Scanner TempConversion

--- src/com/company/TempConversion.java
+++ src/com/company/TempConversion.java
@@ -1,11 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class TempConversion {
 
     public static void main(String [] args){
 
-        double Fahrenheit = 80.0;
-        double Celsius = (Fahrenheit-32)*5.0/9.0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the temperature in Fahrenheit in order to convert it into Celsius.");
+        double Fahrenheit = scan.nextInt();
+        final double Celsius = (Fahrenheit-32)*5.0/9.0;
         System.out.println(Fahrenheit+"°F = "+Celsius+"°C");
     }
 }
