9ee64e99129012bffafe71eb07a7fc03a81fa813
suminhong02 <sumin.hong@ssfs.org>
2019-09-23 08:28:21 -0400

    Completed 1.5 RoundNegatives

--- src/com/company/RoundNegatives.java
+++ src/com/company/RoundNegatives.java
@@ -1,4 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class RoundNegatives {
+
+    public static void main(String [] args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a negative decimal number: ");
+        Double value1 = scan.nextDouble();
+        int value2 = (int)(value1-0.5);
+        System.out.println(value1+" rounded to a whole number is "+value2);
+    }
 }
