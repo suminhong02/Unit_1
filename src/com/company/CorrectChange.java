d8a4e70ae86502bb7bbb7d3a186822d2bfffc81c
suminhong02 <sumin.hong@ssfs.org>
2019-09-24 09:56:17 -0400

    Completed CorrectChange

--- src/com/company/CorrectChange.java
+++ src/com/company/CorrectChange.java
@@ -1,5 +1,8 @@
 package com.company;
-
+/*Sumin Hong
+9/24/2019
+To read change from a user (in cents) and write out how many dollars, quarters, dimes, nickels, and pennies that one is due.
+ */
 import java.util.Scanner;
 
 public class CorrectChange {
@@ -7,17 +10,17 @@ public class CorrectChange {
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter a whole number of cents");
-        int wholenumofcents = scan.nextInt();
-        int numofdollars = wholenumofcents/100;
-        int remaindernumofdollars = wholenumofcents%100;
-        int numofquarters = remaindernumofdollars/25;
-        int remaindernumofquarters = remaindernumofdollars%25;
-        int numofdimes = remaindernumofquarters/10;
-        int remaindernumofdimes = remaindernumofquarters%10;
-        int numofnickels = remaindernumofdimes/5;
-        int remaindernumofnickels = remaindernumofdimes%5;
-        int numofpennies = remaindernumofnickels/1;
-        System.out.println("You have "+numofdollars+ "dollars, "+numofquarters+"quarters, "+numofdimes+"dimes, "+numofnickels+"nickels, "+numofpennies+"pennies.");
+        int wholeNumOfCents = scan.nextInt();
+        int numOfDollars = wholeNumOfCents/100;
+        int remainderNumOfDollars = wholeNumOfCents%100;
+        int numOfQuarters = remainderNumOfDollars/25;
+        int remainderNumOfQuarters = remainderNumOfDollars%25;
+        int numOfDimes = remainderNumOfQuarters/10;
+        int remainderNumOfDimes = remainderNumOfQuarters%10;
+        int numOfNickels = remainderNumOfDimes/5;
+        int remainderNumOfNickels = remainderNumOfDimes%5;
+        int numOfPennies = remainderNumOfNickels/1;
+        System.out.println("You have "+numOfDollars+ " dollars, "+numOfQuarters+" quarters, "+numOfDimes+" dimes, "+numOfNickels+" nickels, "+numOfPennies+" pennies.");
 
     }
 }
